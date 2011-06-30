3f9e8d2802e4a4be2aee9e0085016c0e097329f7
Mitja Kleider <mitja@kleider.name>
2011-06-30 17:55:48 +0200

    import full data

--- import/osm2json.js
+++ import/osm2json.js
@@ -1,106 +1,71 @@
-/* Please note that this version of osm2json creates a multipolygon AND a way or relation if the way/relation is a multipolygon. */
-
-
-var jsonfile;
-var first_object = true;
-
 Osmium.Callbacks.init = function() {
-    jsonfile = Osmium.Output.CSV.open('bulk.json'); // normal file handler, "CSV" contains no separator if print() gets only one argument
+    jsonfile = Osmium.Output.CSV.open('bulk.json');
     jsonfile.print('{"docs":[');
+    
+    timestamp = new Date();
 }
 
 Osmium.Callbacks.end = function() {
+    var output = { _id: 'meta', timestamp: timestamp.toString() }
+    jsonfile.print(JSON.stringify(output)); // make sure there is a document without comma at the end
     jsonfile.print(']}');
     jsonfile.close();
 }
-
 Osmium.Callbacks.node = function() {
     var output = {
         _id: 'node'+this.id,
-        type: "Feature",
-        geometry: {
-            type: "Point",
-            coordinates: this.geom.as_array
-        },
-        properties: {
-            version: this.version,
-            timestamp: this.timestamp,
-            uid: this.uid,
-            user: this.user,
-            changeset: this.changeset,
-            tags: this.tags
-        }
+        geom: this.geom.as_array,
+        version: this.version,
+        timestamp: this.timestamp,
+        uid: this.uid,
+        user: this.user,
+        changeset: this.changeset,
+        tags: this.tags
     };
-    if (first_object) {
-        first_object = false;
-    }
-    else {
-        jsonfile.print(',');
-    }
-    jsonfile.print(JSON.stringify(output));
+    jsonfile.print(JSON.stringify(output) + ',');
 }
 
 Osmium.Callbacks.way = function() {
+    if (is_area(this.tags)) return; // do not output polygons as LineString ways
     var output = {
         _id: 'way'+this.id,
-        type: "Feature",
-        geometry: {
-            type: "LineString",
-            coordinates: this.geom.as_array
-        },
-        properties: {
-            version: this.version,
-            timestamp: this.timestamp,
-            uid: this.uid,
-            user: this.user,
-            changeset: this.changeset,
-            tags: this.tags,
-            nodes: this.nodes
-        }
+        geom: this.geom.as_array,
+        version: this.version,
+        timestamp: this.timestamp,
+        uid: this.uid,
+        user: this.user,
+        changeset: this.changeset,
+        tags: this.tags,
+        nodes: this.nodes
     };
-    jsonfile.print(',');
-    jsonfile.print(JSON.stringify(output));
+    jsonfile.print(JSON.stringify(output) + ',');
 }
 
 Osmium.Callbacks.relation = function() {
+    if (is_multipolygon(this.tags)) return; // do not output MultiPolygons as relations
     var output = {
         _id: 'relation'+this.id,
         members: this.members,
-        properties: {
-            version: this.version,
-            timestamp: this.timestamp,
-            uid: this.uid,
-            user: this.user,
-            changeset: this.changeset,
-            tags: this.tags
-        }
+        version: this.version,
+        timestamp: this.timestamp,
+        uid: this.uid,
+        user: this.user,
+        changeset: this.changeset,
+        tags: this.tags
     };
-    jsonfile.print(',');
-    jsonfile.print(JSON.stringify(output));
+    jsonfile.print(JSON.stringify(output) + ',');
 }
 
 Osmium.Callbacks.multipolygon = function() {
+    if (this.from === 'way' && !is_area(this.tags)) return; // do not output non-area ways as Polygon
     var geom = this.geom.as_array;
     if (geom != undefined) {
-        if (this.from === "way") {
-            type = "Polygon";
-        }
-        else {
-            type = "MultiPolygon";
-        }
         var output = {
-            _id: 'mp_'+this.from+this.id,
-            type: "Feature",
-            geometry: {
-                type: type,
-                coordinates: geom
-            },
-            properties: {
-                timestamp: this.timestamp,
-                tags: this.tags,
-            }
+            _id: this.from+this.id,
+            geom: geom,
+            timestamp: this.timestamp,
+            tags: this.tags
         };
-        jsonfile.print(',');
-        jsonfile.print(JSON.stringify(output));
+        jsonfile.print(JSON.stringify(output) + ',');
     }
 }
