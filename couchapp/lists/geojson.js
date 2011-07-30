4d85d8b4b04949d03c7071b486dc5262061e1604
Mitja Kleider <mitja@kleider.name>
2011-07-30 17:52:59 +0200

    add OR support from poi branch

--- couchapp/lists/geojson.js
+++ couchapp/lists/geojson.js
@@ -1,10 +1,10 @@
 /**
-* Spatial list function, expects GeoJSON geometry as key and properties as value.
-* Outputs a GeoJSON FeatureCollection (compatible with OpenLayers).
-*
-* @author Volker Mische
-* @author Mitja Kleider
-*/
+ * Spatial list function, expects GeoJSON geometry as key and properties as value.
+ * Outputs a GeoJSON FeatureCollection (compatible with OpenLayers).
+ * 
+ * @author Volker Mische
+ * @author Mitja Kleider
+ */
 function(head, req) {
     var row, out, sep = '\n';
 
@@ -22,8 +22,18 @@ function(head, req) {
         for (var key in req.query) {
             if (key === 'bbox')
                 continue; // skip bbox parameter
-            else if (!(row.value[key] && (row.value[key] === req.query[key] || req.query[key] === '' || req.query[key] === '*'))) { // tag is not present
+            else if (!row.value[key] || ((req.query[key].indexOf('|') === -1) && !(row.value[key] === req.query[key] || req.query[key] === '' || req.query[key] === '*'))) { // tag is not present
                     omit = true;
+                    break; // do not check further filter tags
+            }
+            else if(row.value[key] && (req.query[key].indexOf('|') != -1)) { // multiple value options
+                omit = true;
+                var values = req.query[key].split('|');
+                for (var i=0; i<values.length; i++) { // values in OR mode
+                    if (row.value[key] === values[i])
+                        omit = false;
+                }
+                if (omit) // no possible value matched
                     break; // do not check further filter tags
             }
         }
